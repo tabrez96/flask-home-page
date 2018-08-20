49c6f94d866f36f44f938a851488473d91d36e89
Tabrez Basha <tabrez_basha@fulfil.io>
2018-08-21 05:26:22 +0530

    Complete webpage

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Velocity from 'velocity-animate';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
